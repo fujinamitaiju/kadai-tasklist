ab89f500cd99fa11e5efec2ae828f2cc59fd119b
Taiju Fujinami <aaachokichokiaaa@gmail.com>
2017-10-24 00:35:42 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  
+  before_action :set_message, only: [:show,:edit,:update,:destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    
   end
 
   def new
@@ -25,11 +25,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
   end
 
   def update
-    @task = Task.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = 'Task が投稿されました'
       redirect_to @task
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    
     @task.destroy
      flash[:success] = 'Task が投稿されました'
      redirect_to tasks_url
@@ -50,6 +50,10 @@ class TasksController < ApplicationController
   
   private
 
+  def set_task
+    @task=Task.find(params[:id])
+  end
+  
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
